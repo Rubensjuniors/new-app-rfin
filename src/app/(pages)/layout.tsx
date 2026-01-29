fc4f2aba7907ff395fe44cc064d19c3a2ed5706c
rubens junio <rubens.marques@hotmart.com>
2026-01-29 14:36:31 -0300

    feat: add layout and home page components for initial application structure

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,4 +1,4 @@
-import '../client/shared/assets/styles/globals.css'
+import '../../client/shared/assets/styles/globals.css'
 
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
