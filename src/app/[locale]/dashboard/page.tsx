1ae43dc39b2a99cdac22c8fb3da09ee54e1328fd
rubens junio <rubens.marques@hotmart.com>
2026-02-02 08:07:30 -0300

    feat(i18n): update Portuguese translations for sign-in page and general terms

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,18 +1,3 @@
-import { getServerSession } from 'next-auth/next'
-
-import { authOptions } from '@/server/libs/auth'
-
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Bem-vindo, {session?.user?.name || session?.user?.email}!</h1>
-
-      <div className="bg-white rounded-lg shadow p-6 text-black">
-        <h2 className="text-lg font-semibold mb-2">Seus dados:</h2>
-        <pre className="bg-gray-10 p-4 rounded">{JSON.stringify(session, null, 2)}</pre>
-      </div>
-    </main>
-  )
+  return <main className="container mx-auto p-4" />
 }
