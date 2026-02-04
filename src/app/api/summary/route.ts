39e3a8f3c91803191648fa97816039d0d007fab9
rubens junio <rubens.marques@hotmart.com>
2026-02-04 20:39:29 -0300

    feat(api): add artificial delay in GET request for summary endpoint

--- src/app/api/summary/route.ts
+++ src/app/api/summary/route.ts
@@ -3,5 +3,6 @@ import { NextRequest } from 'next/server'
 import { summaryController } from '@/server/controller/SummaryController'
 
 export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 300))
   return summaryController(request)
 }
